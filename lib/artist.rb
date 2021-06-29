605be7250c67389a5a290a59d51335479ddf554b
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-06-29 19:31:06 -0400

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -22,18 +22,20 @@ class Artist
         song.artist = self
     end
 
+
     def self.find_or_create_by_name(name)
         #take the name passed in (a string), and Find the artist instance 
         #that has that name 
         #or create one if it doesn't exist. 
         #Either way, the return value will be an instance of an artist with 
         #the name attribute filled out.
-        @@all.detect do |i|
-            binding.pry
-           # i.name == name
+      #  binding.pry
+        if self.all.include?(name)
+            name
+        else
+            new_creation = Artist.new(name)
+            new_creation
         end
-            #iterate through @@all to find each object name
-           
     end
 
 
